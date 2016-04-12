298d2966cc3a97c63b65e3411d65444835c6d84f
tokahuke <none>
2016-04-12 13:36:51 -0300

    Removed out of date folder.

--- exemples/xmppshowcase/Showcase.java
+++
@@ -1,9 +0,0 @@
-package xmppshowcase;
-
-
-public class Showcase {
-	public static void main(String[] args) {
-		new Thread(new Alice(), "Alice").start();
-		new Thread(new Bob(), "Bob").start();
-	}
-}
